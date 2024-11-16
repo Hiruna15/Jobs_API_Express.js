81b033c25959b70f3f68b6cf35248299c1da67b4
Hiruna15 <hirunadilmith15@gmail.com>
2024-11-16 18:26:44 +0530

    setting up auth middleware | validate JWT

---
+++ middleware/authentication.js
@@ -0,0 +1,23 @@
+import UserModel from "../models/User";
+import jwt from "jsonwebtoken";
+import { UnauthenticatedError } from "../errors/index.js";
+
+const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: decoded.userId, name: decoded.name };
+    next();
+  } catch (err) {
+    throw new UnauthenticatedError("Authentication invalid");
+  }
+};
+
+export default auth;
