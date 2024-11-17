9f86484fc547b62b2fc357868be955bb68651ad1
Hiruna15 <hirunadilmith15@gmail.com>
2024-11-17 19:57:36 +0530

    update errorHandlerMiddleware-01

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,32 @@
-import CustomAPIError from "../errors/custom-api.js";
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  const customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong try aging later",
+  };
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+
+    customError.statusCode = 400;
   }
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${
+      Object.keys(err.keyValue)[0]
+    } field, please choose another value`;
+    customError.statusCode = 400;
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 export default errorHandlerMiddleware;
